16f120a1ae7d9e871218bd9ae7e9b754c91be627
unnoq <contact@unnoq.com>
2025-05-29 19:40:50 +0700

    url

--- src/lib/orpc.ts
+++ src/lib/orpc.ts
@@ -15,7 +15,7 @@ declare global {
 }
 
 const link = new RPCLink({
-  url: new URL('/rpc', typeof window !== 'undefined' ? window.location.href : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'),
+  url: new URL('/rpc', typeof window !== 'undefined' ? window.location.href : process.env.VERCEL_URL ? `https://orpc-next-playground.vercel.app` : 'http://localhost:3000'),
   plugins: [
     new BatchLinkPlugin({
       groups: [{
