8185f45f1ab866bb146fa596517770a9f8fb8490
unnoq <contact@unnoq.com>
2025-06-20 13:26:46 +0700

    new optimized ssr approach

--- src/lib/orpc.ts
+++ src/lib/orpc.ts
@@ -3,30 +3,33 @@ import type { RouterClient } from '@orpc/server'
 import { createORPCClient } from '@orpc/client'
 import { RPCLink } from '@orpc/client/fetch'
 import { createORPCReactQueryUtils } from '@orpc/react-query'
-import { BatchLinkPlugin } from '@orpc/client/plugins'
-
-/**
- * This is part of the Optimize SSR setup.
- *
- * @see {@link https://orpc.unnoq.com/docs/integrations/next#optimize-ssr}
- */
-declare global {
-  var $client: RouterClient<typeof router> | undefined
-}
 
 const link = new RPCLink({
   url: new URL('/rpc', typeof window !== 'undefined' ? window.location.href : process.env.VERCEL_URL ? `https://orpc-next-playground.vercel.app` : 'http://localhost:3000'),
-  plugins: [
-    new BatchLinkPlugin({
-      mode: 'buffered',
-      groups: [{
-        condition: () => true,
-        context: {},
-      }],
-    }),
-  ],
+  headers: async () => {
+    if (typeof window === 'undefined') {
+      const { headers } = await import('next/headers')
+      return Object.fromEntries(await headers())
+    }
+
+    return {}
+  },
+  fetch: async (request, init) => {
+    if (typeof window !== 'undefined') {
+      return fetch(request, init)
+    }
+
+    const { handler } = await import('@/app/rpc/[[...rest]]/route')
+    const { response } = await handler.handle(request, {
+      prefix: '/rpc',
+    })
+
+    console.log('Response:', response)
+
+    return response ?? new Response('Not found', { status: 404 })
+  }
 })
 
-export const client: RouterClient<typeof router> = globalThis.$client ?? createORPCClient(link)
+export const client: RouterClient<typeof router> = createORPCClient(link)
 
 export const orpc = createORPCReactQueryUtils(client)
